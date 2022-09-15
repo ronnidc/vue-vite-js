cbb88b05c453ef05980fc9745a800695635696b6
Ronni Chidekel <ronni@kudo.dk>
2022-09-16 00:02:08 +0200

    add sass

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/helloworld/HelloWorld.vue'
 </script>
 
 <template>
